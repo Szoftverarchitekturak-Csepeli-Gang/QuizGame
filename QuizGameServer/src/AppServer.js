396a7ae44c5aa7ac9724703a177867803167c530
NDani23 <nagydaniel57@gmail.com>
2025-11-10 17:11:13 +0100

    Created basic GET endpoints for Questionbanks
    - Created express restAPI server
    - Created 3 get endpoints for questionbanks:
    - Created QuestionBankDTO on the host side
    - Created client side methods in NetworkManager to call these endpoints

--- QuizGameServer/src/AppServer.js
+++ QuizGameServer/src/AppServer.js
@@ -1,22 +1,12 @@
 const db_client = require('./db/db_client.js')
-const question_bank_DAO = require('./db/question_bank_DAO.js')
+const restAPI = require('./network/rest_server.js')
 
 async function main() {
     await db_client.InitDatabase();
 
-    try{
-      const questionBank = await question_bank_DAO.createQuestionBank(2, "TestBank")
-      console.log(questionBank);
-
-      await question_bank_DAO.updateQuestionBank(questionBank.id, 2, "TestBankModified", true)
-      const questionBanks = await question_bank_DAO.getAllQuestionBanks()
-      console.log(questionBanks);
-
-      await question_bank_DAO.deleteQuestionBank(questionBank.id)
-
-    } catch (error) {
-      console.log(error);     
-    }
+    restAPI.listen(8080, () => {
+    console.log('Server is listening on port 8080...');
+    })
 }
 
 main()
